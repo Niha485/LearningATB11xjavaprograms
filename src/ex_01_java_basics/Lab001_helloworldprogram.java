17b277ad316728823e8338a4eacdbbae6de27b6f
HP <niharikakamandala@gmailcom>
2025-03-10 22:26:11 +0530

    10 mar-Iam pushing code

--- src/ex_01_java_basics/Lab001_helloworldprogram.java
+++ src/ex_01_java_basics/Lab001_helloworldprogram.java
@@ -2,6 +2,6 @@ package ex_01_java_basics;
 
 public class Lab001_helloworldprogram {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("hello");
     }
 }
