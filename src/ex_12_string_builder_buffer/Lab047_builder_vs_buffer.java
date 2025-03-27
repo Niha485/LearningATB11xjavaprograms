56fccc17c299ad6c16fdb7cfa2715c941118b45e
HP <niharikakamandala@gmailcom>
2025-03-27 23:09:04 +0530

    27 mar-Iam pushing code

---
+++ src/ex_12_string_builder_buffer/Lab047_builder_vs_buffer.java
@@ -0,0 +1,17 @@
+package ex_12_string_builder_buffer;
+
+public class Lab047_builder_vs_buffer {
+    public static void main(String[] args) {
+        String n1 = "niha";
+        String n2 = new String("niha"); //        // String - 90%
+
+        StringBuilder stringBuilder = new StringBuilder("niha");
+        StringBuffer stringBuffer = new StringBuffer("niha");    // less than <10% used.
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(stringBuilder);
+        System.out.println(stringBuffer);
+
+    }
+}
